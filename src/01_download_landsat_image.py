389a086003883a85cbf9ead4b5855555de9de68b
srnarvaez <sebnarvaez19@gmail.com>
2023-01-01 23:53:58 -0500

    Download train-test landsat image

---
+++ src/01_download_landsat_image.py
@@ -0,0 +1,90 @@
+# Imports
+import os
+
+import ee
+import geemap
+import geopandas as gpd
+import rasterio
+from rasterio.merge import merge
+from statgis.landsat_functions import landsat_cloud_mask, landsat_scaler
+
+from functions.gee_processing import add_indices, rename_bands
+
+# initialize Earth Engine
+ee.Initialize()
+
+# Define paths to load and save data
+poc_path = "data/external/randomfor_controlpoints_utm.shp"
+save_image = "data/raster/train_test_image"
+save_parts = save_image + "/parts/"
+
+# Add a margin to the images
+margin = 0.04
+
+
+def main():
+    # Load points data
+    poc = geemap.shp_to_ee(poc_path)
+    gdf = gpd.read_file(poc_path).to_crs(epsg=4326)
+
+    # extract from the points the images bounds
+    lims = [
+        gdf.bounds.minx.min() - margin,
+        gdf.bounds.miny.min() - margin,
+        gdf.bounds.maxx.max() + margin,
+        gdf.bounds.maxy.max() + margin,
+    ]
+
+    # Create a rectangle from the bounds and the nest of regions to save
+    # the image
+    bbox = ee.Geometry.BBox(*lims)
+    fnet = geemap.fishnet(bbox, rows=2, cols=2)
+
+    # Load landsat image collection and filter by date and region, also scale
+    # the images and mask the clouds and calculate the spatial indeces related
+    # to vegetation
+    L7 = (
+        ee.ImageCollection("LANDSAT/LE07/C02/T1_L2")
+        .filterBounds(poc)
+        .filterDate("2007-02-01", "2007-03-31")
+        .map(landsat_cloud_mask)
+        .map(landsat_scaler)
+        .map(rename_bands)
+        .map(add_indices)
+    )
+
+    # Reduce the images to the mean
+    img = L7.mean()
+
+    # Download the image by tiles because the large image size
+    geemap.download_ee_image_tiles(
+        img, fnet, save_parts, scale=30, unmask_value=None, crs="EPSG:32618"
+    )
+
+    # Load downloaded raster
+    paths_rasters = [save_parts + file for file in os.listdir(save_parts)]
+    rasters = [rasterio.open(raster) for raster in paths_rasters]
+
+    # Merge the raster in a mosaic
+    mosaic, transform = merge(rasters)
+
+    # Get the metadata of on image and update the width, height and tranform
+    # to save the mosaic
+    meta_data = rasters[0].meta.copy()
+    meta_data.update(
+        {
+            "height": mosaic.shape[1],
+            "width": mosaic.shape[2],
+            "transform": transform,
+        }
+    )
+
+    # Save the mosaic
+    with rasterio.open(save_image + "/landsat_img.tiff", "w", **meta_data) as src:
+        src.write(mosaic)
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
