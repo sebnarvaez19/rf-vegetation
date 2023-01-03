ca06d1f8df8b33a7857b23188694e78a579ac5f7
srnarvaez <sebnarvaez19@gmail.com>
2023-01-02 21:30:51 -0500

    Add function to add a DEM to an image

--- src/functions/gee_processing.py
+++ src/functions/gee_processing.py
@@ -53,6 +53,55 @@ def add_indices(image: ee.Image) -> ee.Image:
     }
 
     for name, formula in indices.items():
-        image = image.addBands(image.expression(formula).toFloat().rename(name))
+        image = image.addBands(
+            image.expression(formula).toFloat().rename(name), None, True
+        )
+
+    return image
+
+
+def add_dem(image: ee.Image, slope: bool = False, dem: str = "SRTM_30m") -> ee.Image:
+    """
+    Function to add a Digital Elevation Model (DEM) to an image.
+
+    Paremeters
+    ----------
+    image : ee.Image
+        Image of interest.
+
+    slope : bool, optional (False)
+        If True, calculate the slope in deegres and add it to the image.
+
+    dem : str, optional ("SRTM_30m")
+        key to select the dem to add:
+        - "SRTM_30m": USGS SRTM DEM with gaps (30 m).
+        - "SRTM_90m": USGS SRTM DEM with gaps filled (90 m).
+        - "HydroSHED_15arc": WWF HydroSHED DEM (~470 m).
+        - "GTOPO_30arc": USGS GTOPO DEM (~1 km).
+
+    Returns
+    -------
+    image :ee.Image
+        Image with the DEM band (and Slope if it was required).
+    """
+
+    match dem:
+        case "SRTM_30m":
+            dem_path = "USGS/SRTMGL1_003"
+        case "SRTM_90m":
+            dem_path = "CGIAR/SRTM90_V4"
+        case "HydroSHED_15arc":
+            dem_path = "WWF/HydroSHEDS/15CONDEM"
+        case "GTOPO_30arc":
+            dem_path = "USGS/GTOPO30"
+        case _:
+            Exception("INVALID DEM, see the options in the docstring")
+
+    dem = ee.Image(dem_path).rename("DEM")
+    image = image.addBands(dem, None, True)
+
+    if slope:
+        slope = ee.Terrain.slope(dem).rename("SLOPE")
+        image = image.addBands(slope, None, True)
 
     return image
