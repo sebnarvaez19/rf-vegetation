c3c0e423cb321d92a29316017d301c6256e05bab
srnarvaez <sebnarvaez19@gmail.com>
2023-01-04 16:11:24 -0500

    function to plot a map of the samples

--- src/functions/gis_utils.py
+++ src/functions/gis_utils.py
@@ -1,7 +1,12 @@
 import geopandas as gpd
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import rasterio
+import xarray
+
+from matplotlib.axes import Axes
+from typing import Sequence
 
 
 def sample_points(
@@ -34,3 +39,82 @@ def sample_points(
     sample = gpd.GeoDataFrame(sample[sample.columns[:-1]], geometry=geom)
 
     return sample
+
+
+def plot_samples(
+    samples: gpd.GeoDataFrame,
+    variable: str,
+    raster: xarray.Dataset,
+    bands: list[int] | int = 1,
+    zlims: Sequence[float] | None = None,
+    ylims: Sequence[float] | None = None,
+    xlims: Sequence[float] | None = None,
+    ax: Axes | None = None,
+) -> Axes:
+    """
+    Plotthe sample in a raster opened with rioxarray.
+
+    Parameters
+    ----------
+    data : geopandas.GeoDataFrame
+        Data frame with the samples data
+
+    variable : str
+        Variable of interest
+
+    raster : xarray.Dataset
+        Raster.
+
+    bands : Sequence[int] | int (optional) = 1
+        Raster bands to plot.
+
+    zlims : Sequence[float] | None (optional) = None
+        Limits of raster values.
+
+    ylims : Sequence[float] | None (optional) = None
+        Y-Axis limits.
+
+    xlims : Sequence[float] | None (optional) = None
+        X-Axis limits.
+
+    ax : matplotlib.axes.Axes | None (optional) = None
+        Axes to draw the plot.
+
+    Returns
+    -------
+    ax : matplotlib.axes.Axes
+        Map.
+    """
+    
+    # Squeez bands
+    raster = raster.sel(band=bands)
+    raster = raster.squeeze()
+
+    # If there not axes, create one
+    if ax == None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+    # If there are not zlims, plot raster without them
+    if zlims == None:
+        raster.plot.imshow(ax=ax)
+    else:
+        raster.plot.imshow(vmin=zlims[0], vmax=zlims[1], ax=ax)
+
+    # Add the samples
+    for sample in samples[variable].unique():
+        samples[samples[variable] == sample].plot(label=sample, ax=ax, alpha=0.5)
+    
+    # Add the legend
+    ax.legend(title=variable, loc=0, frameon=True, framealpha=0.5)
+
+    # Remove default title
+    ax.set_title(None)
+
+    if ylims != None:
+        ax.set_ylim(ylims[0], ylims[1])
+
+    if xlims != None:
+        ax.set_xlim(xlims[0], xlims[1])
+        
+    return ax
