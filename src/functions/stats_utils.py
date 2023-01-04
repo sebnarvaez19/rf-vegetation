f3392c8be69e7f9fc3fd829c6794e3e57ae278d2
srnarvaez <sebnarvaez19@gmail.com>
2023-01-04 16:11:08 -0500

    change functionality of plots to return axes

--- src/functions/stats_utils.py
+++ src/functions/stats_utils.py
@@ -3,13 +3,17 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib.figure import Figure
+from matplotlib.axes import Axes
 from numpy.typing import ArrayLike
 from sklearn.decomposition import PCA
 
 
 def plot_variable_importance(
-    X: ArrayLike, y: pd.Series, labels: pd.Series | None = None
-) -> Figure:
+    X: ArrayLike, 
+    y: pd.Series, 
+    labels: pd.Series | None = None, 
+    ax: Axes | None = None,
+) -> Axes:
     """
     Plot a PCA with the two forst component to show what variables in X are more
     important to define y.
@@ -25,9 +29,12 @@ def plot_variable_importance(
     labels : pandas.Series | None (Optional) = None
         Labels for dependent variable.
 
+    ax : matplotlib.axes.Axes | None (optional) = None
+        Axes to draw the plot.
+
     Returns
     -------
-    fig : matplotlib.figure.Figure
+    ax : matplotlib.axes.Axes
         Scatter of PCA.
     """
 
@@ -44,35 +51,36 @@ def plot_variable_importance(
     xcp = np.sqrt(0.5**2 - xc**2)
     xcn = -np.sqrt(0.5**2 - xc**2)
 
-    # Create figure
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1, 1, 1)
+    if ax == None:
+        # Create figure
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
 
     # Add guides
-    ax1.axhline(0, color="black", lw=0.2)
-    ax1.axvline(0, color="black", lw=0.2)
-    ax1.plot(xc, xcp, color="red", lw=0.2)
-    ax1.plot(xc, xcn, color="red", lw=0.2)
+    ax.axhline(0, color="black", lw=0.2)
+    ax.axvline(0, color="black", lw=0.2)
+    ax.plot(xc, xcp, color="red", lw=0.2)
+    ax.plot(xc, xcn, color="red", lw=0.2)
 
     # Add values by y
     for yi, label in zip(y.unique(), labels.unique()):
         mask = y == yi
-        ax1.scatter(val[mask, 0], val[mask, 1], alpha=0.4, label=label)
+        ax.scatter(val[mask, 0], val[mask, 1], alpha=0.4, label=label)
 
     # Add vectors to show the importnace of the variables in X
     for var, delta in zip(X.columns, com):
-        ax1.arrow(0, 0, delta[0], delta[1], color="black")
-        ax1.text(delta[0], delta[1], var, va="center", ha="center", size=6)
+        ax.arrow(0, 0, delta[0], delta[1], color="black")
+        ax.text(delta[0], delta[1], var, va="center", ha="center", size=6)
 
     # Set lims and labels
-    ax1.set(xlim=(-0.7, 0.7), ylim=(-0.7, 0.7))
-    ax1.set_xlabel(f"Component 1: {pca.explained_variance_ratio_[1]:0.3f}")
-    ax1.set_ylabel(f"Component 2: {pca.explained_variance_ratio_[0]:0.3f}")
+    ax.set(xlim=(-0.7, 0.7), ylim=(-0.7, 0.7))
+    ax.set_xlabel(f"Component 1: {pca.explained_variance_ratio_[1]:0.3f}")
+    ax.set_ylabel(f"Component 2: {pca.explained_variance_ratio_[0]:0.3f}")
 
     # Add legend in uper-right corner
-    ax1.legend(loc=1)
+    ax.legend(loc=1)
 
-    return fig
+    return ax
 
 
 def group_boxplot(
@@ -82,7 +90,8 @@ def group_boxplot(
     title: str | None = None,
     ticklabels: ArrayLike | None = None,
     showfliers: bool = True,
-) -> Figure:
+    ax: Axes | None = None,
+) -> Axes:
     """
     Function to melt variable and plot it on a boxplot.
 
@@ -106,31 +115,37 @@ def group_boxplot(
     showfliers : bool (optional) = True
         Show outliers in boxplot.
 
+    ax : matplotlib.axes.Axes | None (optional) = None
+        Axes to draw the plot.
+
     Returns
     -------
-    fig : matplotlib.figure.Figure
+    ax : matplotlib.axes.Axes
         Boxplot.
     """
 
     # Melt data to long format
     df = pd.melt(data, id_vars=y, value_vars=variables)
 
-    # Create figure and axes
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1, 1, 1)
+    if ax == None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
 
     # Plot data
-    sns.boxplot(data=df, y="value", x="variable", hue=y, showfliers=showfliers, ax=ax1)
+    sns.boxplot(data=df, y="value", x="variable", hue=y, showfliers=showfliers, ax=ax)
 
     # Hide unnecesary label
-    ax1.set_xlabel(None)
+    ax.set_xlabel(None)
+
+    # Hide legend by default
+    ax.legend().set_visible(False)
 
     # If there is a title, use it
     if title != None:
-        ax1.set_title(title)
+        ax.set_title(title)
 
     # If there are ticklabels, use them
     if ticklabels != None:
-        ax1.set_xticklabels(ticklabels)
+        ax.set_xticklabels(ticklabels)
 
-    return fig
+    return ax
